e4c24648de75906e281729eb35e185c9ceddc164
Nguyen The Hoang <you@example.com>
2025-03-21 20:37:15 +0700

    Fix env typo, re-add env.production

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly SUPER_TODO_API_URL: string
+    readonly VITE_SUPER_TODO_API_URL: string
 }
   
 interface ImportMeta {
